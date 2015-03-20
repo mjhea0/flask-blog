1587f237128f783c0915b587aa2140d7188e6ba0
JoshuaAcosta <Acosta.joshua03@gmail.com>
2015-03-19 22:07:48 -0400

    login forms

--- blog.py
+++ blog.py
@@ -4,7 +4,11 @@ from flask import Flask, render_template, request, session, \
 flash, redirect, url_for, g
 import sqlite3
 
+#configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_Key = 'hard_to_guess'
 
 app= Flask(__name__)
 
@@ -15,11 +19,25 @@ def connect_db():
 
 @app.route('/')
 def login():
-	return render_template('login.html')
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or \
+			request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Credentials. Please try again.'
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main'))
+	return render_template('login.html', error = error)
 
 @app.route('/main')
 def main():
 	return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', NONE)
+	flash('You were logged out')
+	return redirect(url_for('login'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
