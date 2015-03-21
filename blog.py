573677dfe6e40e790ff2ab1e740cf758f4aecd19
JoshuaAcosta <Acosta.joshua03@gmail.com>
2015-03-21 08:50:44 -0400

    added login wrapper and show posts

--- blog.py
+++ blog.py
@@ -1,8 +1,9 @@
 #blog.py controller
 
 from flask import Flask, render_template, request, session, \
-flash, redirect, url_for, g
+	flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 #configuration
 DATABASE = 'blog.db'
@@ -17,7 +18,17 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in sessions:
+			return test (*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
 	error = None
 	if request.method == 'POST':
@@ -30,14 +41,22 @@ def login():
 	return render_template('login.html', error = error)
 
 @app.route('/main')
+@login_required
 def main():
-	return render_template('main.html')
+	g.db = connect_db()
+	cur = g.db.execute('select * from posts')
+	posts = [dict(title=row[0], post=row[1]) for now in
+		cur.fetchall()]
+	g.db.close()
+	return render_template('main.html', posts=posts)
 
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', NONE)
+	session.pop('logged_in', None)
 	flash('You were logged out')
 	return redirect(url_for('login'))
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
