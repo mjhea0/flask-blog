c880bbc0d5956cad43c9723e468fc4faa3e75a49
JoshuaAcosta <Acosta.joshua03@gmail.com>
2015-03-22 13:26:52 -0400

    corrected syntax mistakes

--- blog.py
+++ blog.py
@@ -44,17 +44,11 @@ def login():
 @login_required
 def main():
 	g.db = connect_db()
-	cur = g.db.execute('select * from posts')
+	cur = g.db.execute('SELECT * from posts')
 	posts = [dict(title=row[0], post=row[1]) for now in
 		cur.fetchall()]
 	g.db.close()
 	return render_template('main.html', posts=posts)
-
-@app.route('/logout')
-def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out')
-	return redirect(url_for('login'))
 
 @app.route('/add', methods=['POST'])
 @login_required
@@ -62,17 +56,22 @@ def add():
 	title = request.form['title']
 	post = request.form['post']
 	if not title or not post:
-		flask("All fields are required. Please try again.")
+		flash("All fields are required. Please try again.")
 		return redirect(url_for('main'))
 	else:
 		g.db = connect_db()
-		g.db.execute('insert into posts (title, post) values (?,?)',
+		g.db.execute('INSERT INTO posts (title, post) VALUES (?,?)',
 		  [request.form['title'], request.form['post']])
 		g.db.commit()
 		g.db.close()
 		flash('New entry was successfully posted!')
 		return redirect(url_for('main'))
 
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
