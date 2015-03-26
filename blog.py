2d2c50cac82d025b678d11a20748ef06ec3620a9
Michael Herman <hermanmu@gmail.com>
2015-03-25 18:25:15 -0600

    updated type, pep8 issues

--- blog.py
+++ blog.py
@@ -1,78 +1,83 @@
-#blog.py controller
+# blog.py controller
 
 from flask import Flask, render_template, request, session, \
-	flash, redirect, url_for, g
+    flash, redirect, url_for, g
 import sqlite3
 from functools import wraps
 
-#configuration
+# configuration
 DATABASE = 'blog.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 SECRET_KEY = 'hard_to_guess'
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 app.config.from_object(__name__)
 
+
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
+
 
 def login_required(test):
-	@wraps(test)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return test (*args, **kwargs)
-		else:
-			flash('You need to login first.')
-			return redirect(url_for('login'))
-	return wrap
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME'] or \
-			request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid Credentials. Please try again.'
-		else:
-			session['logged_in'] = True
-			return redirect(url_for('main'))
-	return render_template('login.html', error = error)
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
 
 @app.route('/main')
 @login_required
 def main():
-	g.db = connect_db()
-	cur = g.db.execute('SELECT * from posts')
-	posts = [dict(title=row[0], post=row[1]) for now in
-		cur.fetchall()]
-	g.db.close()
-	return render_template('main.html', posts=posts)
+    g.db = connect_db()
+    cur = g.db.execute('SELECT * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in
+             cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
+
 
 @app.route('/add', methods=['POST'])
 @login_required
 def add():
-	title = request.form['title']
-	post = request.form['post']
-	if not title or not post:
-		flash("All fields are required. Please try again.")
-		return redirect(url_for('main'))
-	else:
-		g.db = connect_db()
-		g.db.execute('INSERT INTO posts (title, post) VALUES (?,?)',
-		  [request.form['title'], request.form['post']])
-		g.db.commit()
-		g.db.close()
-		flash('New entry was successfully posted!')
-		return redirect(url_for('main'))
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('INSERT INTO posts (title, post) VALUES (?,?)',
+                     [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
+
 
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out')
-	return redirect(url_for('login'))
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
-	app.run(debug=True)
-
+    app.run(debug=True)
--- sql.py
+++ sql.py
@@ -1,18 +1,16 @@
 import sqlite3
 
 with sqlite3.connect("blog.db") as connection:
-	#create cursor object used to execute SQL commands
-	c = connection.cursor()
+    # create cursor object used to execute SQL commands
+    c = connection.cursor()
 
-	#create the table
-	c.execute("""CREATE TABLE posts 
-		     (title TEXT, post TEXT)
-		     """)
+    # create the table
+    c.execute("""CREATE TABLE posts
+             (title TEXT, post TEXT)
+             """)
 
-	#enter dummy data into table
-	c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
-	c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
-	c.execute('INSERT INTO posts VALUES("Excellent", "I\'m execellent.")')
-	c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
-
-
+    # enter dummy data into table
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
+    c.execute('INSERT INTO posts VALUES("Excellent", "I\'m execellent.")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
